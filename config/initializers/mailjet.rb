38e32d04a9878e07f6b4e7421316241f482e80b6
cboris <b.crochet@gmail.com>
2023-05-16 22:49:16 +0200

    update

--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -2,7 +2,7 @@
 Mailjet.configure do |config|
   config.api_key = ENV['MAILJET_API_KEY']
   config.secret_key = ENV['MAILJET_API_SECRET']
-  config.default_from = 'dylan.test.mailer@gmail.com'
+  config.default_from = 'b.crochet@gmail.com'
   # Mailjet API v3.1 is at the moment limited to Send API.
   # We’ve not set the version to it directly since there is no other endpoint in that version.
   # We recommend you create a dedicated instance of the wrapper set with it to send your emails.
